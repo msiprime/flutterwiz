f180671671235c21e0a1bc959ec102074dca0d20
msiprime <msisakib958@gmail.com>
2024-08-18 21:51:32 +0600

    this repo will be used for my wiz hunting and personal research ground.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_exploring/text_style.dart';
 
-import 'home_screen.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: MyApp(),
-      title: 'Flutter Exploration',
-    ),
-  );
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return HomeScreen1();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
   }
 }
