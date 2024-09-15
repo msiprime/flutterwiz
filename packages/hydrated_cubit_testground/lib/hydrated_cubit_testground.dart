1d57bda0132b0e4d335536865baabaaec3d0c174
msiprime <msisakib958@gmail.com>
2024-09-15 23:49:22 +0600

    hydrated cubit deleted, cause bloc more imp, hydrated bloc ofc

--- packages/hydrated_cubit_testground/lib/hydrated_cubit_testground.dart
+++
@@ -1,4 +0,0 @@
-/// A Very Good Project created by Very Good CLI.
-library;
-
-export 'src/hydrated_cubit_testground.dart';
--- packages/hydrated_cubit_testground/lib/src/hydrated_cubit_testground.dart
+++
@@ -1,7 +0,0 @@
-/// {@template hydrated_cubit_testground}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class HydratedCubitTestground {
-  /// {@macro hydrated_cubit_testground}
-  const HydratedCubitTestground();
-}
