c31c0e827914ced86800276b678ff2a227751b05
msiprime <msisakib958@gmail.com>
2024-10-15 23:12:18 +0600

    bloc concurrency is discovered... in depth

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'package:rxdart/rxdart.dart';
 import 'package:user_repository/user_repository.dart';
 
 void main() async {
-  // await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   HydratedBloc.storage = await HydratedStorage.build(
