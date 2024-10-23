c42c57ebc374dfb429eefb3b438fd2e15e01b47a
msiprime <msisakib958@gmail.com>
2024-10-23 11:57:29 +0600

    bloc testing learning

--- lib/features/counter_bloc/pages/counter_bloc_screen.dart
+++ lib/features/counter_bloc/pages/counter_bloc_screen.dart
@@ -7,28 +7,42 @@ class CounterBlocScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
+    return BlocProvider<CounterBloc>(
       create: (context) => CounterBloc(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('CounterBlocScreen'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            context.read<CounterBloc>().add(CounterIncremented());
-          },
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-            ],
-          ),
+      child: const CounterScreenView(),
+    );
+  }
+}
+
+class CounterScreenView extends StatelessWidget {
+  const CounterScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterBlocScreen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<CounterBloc>().add(CounterIncremented());
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<CounterBloc, int>(
+              builder: (context, state) {
+                return Text(
+                  '$state',
+                  style: Theme.of(context).textTheme.displayMedium,
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
---
+++ test/Features/counter_bloc/bloc/counter_bloc_test.dart
@@ -0,0 +1,15 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_exploring/features/counter_bloc/bloc/counter_bloc.dart';
+
+main() {
+  blocTest<CounterBloc, int>(
+    'TODO: description',
+    build: () => CounterBloc(),
+    act: (bloc) {
+      // TODO: implement
+    },
+    expect: () => <CounterState>[
+      // TODO: implement
+    ],
+  );
+}
