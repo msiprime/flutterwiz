a9c801e060ad4c596a8f079302f4aa6aa361156e
msiprime <msisakib958@gmail.com>
2024-10-23 12:32:31 +0600

    basic bloc testing done. with counter

--- test/Features/counter_bloc/bloc/counter_bloc_test.dart
+++ test/Features/counter_bloc/bloc/counter_bloc_test.dart
@@ -1,15 +1,39 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_exploring/features/counter_bloc/bloc/counter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 main() {
-  blocTest<CounterBloc, int>(
-    'TODO: description',
-    build: () => CounterBloc(),
-    act: (bloc) {
-      // TODO: implement
-    },
-    expect: () => <CounterState>[
-      // TODO: implement
-    ],
-  );
+  group('counter bloc', () {
+    blocTest<CounterBloc, int>(
+      'initial state',
+      build: () => CounterBloc(),
+      act: (bloc) {
+        // initial event -> state 0
+      },
+      expect: () => <int>[],
+    );
+
+    blocTest<CounterBloc, int>(
+      'After first increment event',
+      build: () => CounterBloc(),
+      act: (bloc) {
+        bloc.add(CounterIncremented());
+      },
+      expect: () => <int>[
+        1,
+      ],
+    );
+    blocTest<CounterBloc, int>(
+      'After second increment event',
+      build: () => CounterBloc(),
+      act: (bloc) {
+        bloc.add(CounterIncremented());
+        bloc.add(CounterIncremented());
+      },
+      expect: () => <int>[
+        1,
+        2,
+      ],
+    );
+  });
 }
