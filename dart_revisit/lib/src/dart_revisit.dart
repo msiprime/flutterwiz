d83fc823c5db79fe5a0d7b7ef58515817decdfea
msiprime <msisakib958@gmail.com>
2024-12-08 21:27:46 +0600

    dart package added for revisiting

---
+++ dart_revisit/lib/src/dart_revisit.dart
@@ -0,0 +1,7 @@
+/// {@template dart_revisit}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class DartRevisit {
+  /// {@macro dart_revisit}
+  const DartRevisit();
+}
---
+++ dart_revisit/test/src/dart_revisit_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:dart_revisit/dart_revisit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('DartRevisit', () {
+    test('can be instantiated', () {
+      expect(DartRevisit(), isNotNull);
+    });
+  });
+}
