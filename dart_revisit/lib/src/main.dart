0afccd1c25b94a42f86921f5932871e3e6d7415c
msiprime <msisakib958@gmail.com>
2024-12-11 02:00:42 +0600

    dart package added for revisiting

--- dart_revisit/lib/src/main.dart
+++ dart_revisit/lib/src/main.dart
@@ -1,19 +1,62 @@
-void main(List<String> args) {
-  const letters = <String>[
-    'a',
-    'b',
-    'c',
-  ];
+//ignore_for_file: avoid_print
+//ignore_for_file: public_member_api_docs
 
-  /// correct way to update a list
-  /// both `letters` and `updatedLetters` are immutable
-  /// works with both `const` and `final`
+void main() {
+  final obj1 = Transaction(
+    name: 'Msi Sakib',
+    age: '27',
+    id: 'Asa12AssocZfeAds',
+    transactionId: '3AsxDawn13as2',
+  );
 
-  const updatedLetters = [...letters, 'd'];
+  final obj2 = Transaction(
+    name: 'Msi Sakib',
+    age: '28',
+    id: 'Asa12AssocZfeAds',
+    transactionId: '3AsxDawn13as2',
+  );
 
-  print(updatedLetters); // [a, b, c, d]
+  final onj3 = Transaction(
+    name: 'Msi Sakib',
+    age: '29',
+    id: 'Asa12AssocZfeAds',
+    transactionId: '3AsxDawn13as2',
+  );
 
-  /// incorrect way to update a list
-  /// `letters` is immutable but `updatedLetters` is mutable
-  /// works with only `final`
+  print(obj1.hashCode == obj2.hashCode);
+  print(obj2 == onj3);
+
+  print('Object 1 hashcode: ${obj1.hashCode}');
+  print('Object 2 hashcode: ${obj2.hashCode}');
+  print('Object 3 hashcode: ${onj3.hashCode}');
+}
+
+// String? doSomething(String? a, String? b) {
+//   return a != null
+//       ? a.isEmpty
+//           ? a = 'A is empty'
+//           : a = 'A is not empty'
+//       : a ??= b;
+// }
+
+class Transaction {
+  Transaction({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.transactionId,
+  });
+
+  String id;
+  String name;
+  String age;
+  String transactionId;
+
+  @override
+  int get hashCode => name.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    return hashCode == other.hashCode;
+  }
 }
