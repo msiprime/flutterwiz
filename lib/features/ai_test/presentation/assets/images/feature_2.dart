5ad2fba81ad1f7fb4742d3b6defeb3579ee24380
msiprime <msisakib958@gmail.com>
2025-03-07 23:28:46 +0600

    learned cool things about interface

--- lib/features/ai_test/presentation/assets/images/feature_2.dart
+++ lib/features/ai_test/presentation/assets/images/feature_2.dart
@@ -9,3 +9,18 @@ class AxyZ extends X implements Y, Z {}
 class B extends Y {}
 
 class C extends B {}
+
+interface class DummyInterface {
+  void doSomething() {
+    print("Doing something");
+  }
+}
+
+abstract class DummyAbstract {
+  void doSomethingElse();
+  void doSomething() {
+    print("Doing something");
+  }
+}
+
+abstract interface class TrueAbstraction {}
